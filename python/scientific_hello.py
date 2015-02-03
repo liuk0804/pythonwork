ecdd47752ff5337dd67a262a1787b89611d12673
Khalid Ahmed <nkahm@umich.edu>
2015-02-03 12:55:40 -0500

    Initial Commit from Research computing seminar 2013

---
+++ python/scientific_hello.py
@@ -0,0 +1,4 @@
+import sys, math
+r = float(sys.argv[1])
+s = math.sin(r)
+print("hello world!", "sin(" , r, ") =", s)
---
+++ python/script.py
@@ -0,0 +1,10 @@
+import sys
+
+print(type(sys.argv))
+
+script, one, two = sys.argv
+
+script = sys.argv[1]
+one = sys.argv[2]
+
+print(script)
